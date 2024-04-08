47e5c23c86adaeabcaf2566229a2268230fbe639
TheRealAsciiMan <147166455+TheRealAsciiMan@users.noreply.github.com>
2024-04-08 15:25:56 +0200

    En cours

---
+++ Test.py
@@ -0,0 +1,12 @@
+from string import *
+
+for i in range(13): print(i%5+1, end='')
+
+print("")
+
+for i in range(200): print(ascii_lowercase[i%26], end='')
+
+print("\n")
+
+print("1234512345123")
+print("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr")
---
+++ cesar.py
@@ -0,0 +1,22 @@
+def code_cesar(texte, decalage):
+    """
+    Fonction qui code un texte suivant la méthode de césar (chaque lettre est
+    décalé).
+    Arguments ::
+        - texte : message à coder de type str
+        - decalage : indice de décalage de type int
+    Renvoie ::
+        - texte_chiffre : le message codé de type str
+    """
+    return
+
+
+def decode_cesar(texte, decalage):
+    """
+    Fonction qui décode un texte suivant la méthode de césar.
+    Arguments ::
+        - texte : message codé de type str
+        - decalage : indice de décalage de type int
+    Renvoie ::
+        - le message décodé de type str
+    """
